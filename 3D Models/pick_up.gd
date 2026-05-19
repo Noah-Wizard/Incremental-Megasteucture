36e4cfd2ce3374393dbda92118eaf4ed13eb67ee
Noah-Wizard <noah@noahwizard.com>
2026-05-18 22:40:26 -0700

    fussed w/lighting. pickup checks for player.

--- 3D Models/pick_up.gd
+++ 3D Models/pick_up.gd
@@ -2,6 +2,6 @@ extends Area3D
 
 signal picked_up
 
-func _on_body_entered(_body: Node3D) -> void:
-	#if body == player:
-	picked_up.emit()
+func _on_body_entered(body: Node3D) -> void:
+	if body is Player:
+		picked_up.emit()
